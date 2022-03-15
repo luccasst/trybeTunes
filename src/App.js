d0849cf446c84a1ca2fa954b0b3bac9b3368f30a
lucas <lucas.c.r.f@hotmail.com>
2022-03-14 21:35:32 -0300

    requisito 7, ufa ^^

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route path="/profile/edit" component={ ProfileEdit } />
-            <Route path="/album/:id" component={ Album } />
+            <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
             <Route path="/profile" component={ Profile } />
             <Route path="/favorites" component={ Favorites } />
             <Route path="/search" component={ Search } />
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Music from './Music';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      musicTrack: '',
+      musics: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getMusicAlbum();
+  }
+
+  getMusicAlbum = async () => {
+    const { match: { params: { id } } } = this.props;
+    const musics = await getMusics(id);
+    const song = musics.filter((track) => track.trackId);
+    console.log(song);
+    this.setState({
+      musicTrack: musics,
+      musics: song,
+    });
+  }
+
+  musicAlbum() {
+    const { musicTrack } = this.state;
+    if (musicTrack) {
+      const { artistName, collectionName, artworkUrl100 } = musicTrack[0];
+      return (
+        <section>
+          <img src={ artworkUrl100 } alt={ artistName } />
+          <h2 data-testid="artist-name">{ artistName }</h2>
+          <h3 data-testid="album-name">{collectionName}</h3>
+        </section>
+      );
+    }
+  }
+
   render() {
+    const { musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        {this.musicAlbum()}
+        { !musics ? (<h2>Carregando...</h2>
+        ) : (
+          musics.map((music) => (
+            <Music
+              key={ music.artistId }
+              artwork={ music.artwork }
+              trackId={ music.trackId }
+              artist={ music.artist }
+              trackName={ music.trackName }
+              previewUrl={ music.previewUrl }
+            />
+          ))
+        )}
       </div>
-
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }),
+}.isRequired;
 
 export default Album;
